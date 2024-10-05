108b63f2a4f4604ab7870667868ebe914a778c0f
ekoo <ekoo@localhost.localdomain>
2024-10-05 17:47:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Homepage from "./pages/Homepage"
+
+function App() {
+
+
+  return (
+   <div>
+    {/* Navabar */}
+    <Navbar />
+    {/* Content */}
+    <Homepage />
+
+    {/* Footer */}
+    {/* <Footer /> */}
+
+   </div>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className="h-[2000px]">Homepage</div>
+  )
+}
+
+export default Homepage
