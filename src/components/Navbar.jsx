b8e4e217e4084b0e000d0e1e671caa4022b87a02
ekoo <ekoo@localhost.localdomain>
2024-10-06 17:41:23 +0700

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from 'react';
 
 const Navbar = () => {
     const [show, setShow] = useState(false);
     const [scroll, setScroll] = useState(false);
+    const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
     const handleClick = () => {
         setShow(!show);
-        // console.log(show);
+        setIsHamburgerActive(!isHamburgerActive);
     };
 
     let menuActive = show ? "left-0" : "-left-full";
+
     useEffect(() => {
         window.addEventListener("scroll", () => {
             if (window.scrollY > 5) {
-                // console.log("test");
                 setScroll(true);
                 setShow(false);
+                setIsHamburgerActive(false);
             } else {
                 setScroll(false);
             }
-        })
+        });
     });
 
     let scrollActive = scroll ? "py-6 bg-white" : "py-4";
+
     return (
         <div className={`navbar fixed w-full transition-all ${scrollActive}`}>
             <div className="container mx-auto px-4">
@@ -30,33 +33,40 @@ const Navbar = () => {
                     <div className="logo">
                         <h1 className='sm:text-2xl text-xl font-bold'>Landingpage</h1>
                     </div>
+
                     <ul className={`flex lg:gap-12 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:text-black md:p-0 md:m-0 md:transition-none gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-sky-400 font-bold text-white transition-all`}>
                         <li className="flex items-center gap-3">
-                            <i class="ri-home-2-line text-3xl md:hidden block"></i>
+                            <i className="ri-home-2-line text-3xl md:hidden block"></i>
                             <a href="#" className='font-medium opacity-75'>Beranda</a>
                         </li>
                         <li className="flex items-center gap-3">
-                            <i class="ri-information-line text-3xl md:hidden block"></i>
+                            <i className="ri-information-line text-3xl md:hidden block"></i>
                             <a href="#" className='font-medium opacity-75'>Tentang Kami</a>
                         </li>
                         <li className="flex items-center gap-3">
-                            <i class="ri-settings-3-line text-3xl md:hidden block"></i>
+                            <i className="ri-settings-3-line text-3xl md:hidden block"></i>
                             <a href="#" className='font-medium opacity-75'>Layanan</a>
                         </li>
                         <li className="flex items-center gap-3">
-                            <i class="ri-image-line text-3xl md:hidden block"></i>
+                            <i className="ri-image-line text-3xl md:hidden block"></i>
                             <a href="#" className='font-medium opacity-75'>Proyek</a>
                         </li>
                     </ul>
+
                     <div className="social flex items-center gap-2">
                         <a href="#" className='bg-sky-400 px-5 py-2 rounded-full text-white font-bold hover:bg-sky-500 transition-all'>Social Media</a>
-                        <i className="ri-menu-line text-3xl md:hidden block" onClick={handleClick}></i>
+                        <div
+                            className={`hamburger md:hidden block ${isHamburgerActive ? "hamburger-active" : ""}`}
+                            onClick={handleClick}>
+                            <span className="hamburger-line w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out origin-top-left"></span>
+                            <span className="hamburger-line w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out"></span>
+                            <span className="hamburger-line w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out origin-bottom-left"></span>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import HeroImage from "../assets/images/hero.svg"
 
 const Homepage = () => {
   return (
-    <div className="h-[2000px]">Homepage</div>
+    <div className="homepage pb-10">
+        <div className="container mx-auto px-4">
+            <div className="hero grid md:grid-cols-2 grid-cols-1 items-center gap-20 pt-32">
+                <div className="box">
+                    <h1 className="lg:text-5xl/tight text-3xl font-medium mb-2 ">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span className="font-bold text-sky-400 underline">Tailwind CSS</span>
+                    </h1>
+                    <p className="text-base/8 mb-7">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto voluptatum rerum enim beatae cupiditate at hic porro laboriosam maxime. Culpa sit sed deserunt corrupti temporibus modi neque nobis magnam porro.</p>
+                    <a href="#" className="bg-sky-400 hover:bg-sky-500 transition-all py-2 px-4 text-white shadow rounded-full">Tentang Kami <i className="ri-eye-line ms-1"></i>
+                    </a>
+                </div>
+                <div className="box">
+                    <img src={HeroImage} alt="Hero Image" className="md:w-full w-[400px] mx-auto md:m-0"/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
